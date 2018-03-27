64b3b2de1ddebf3d1ab4c624253cdb9eeac2a110
Pito Salas <pitosalas@gmail.com>
2018-03-27 17:36:01 -0400

    tiny

--- experiments/rc2.rb
+++ experiments/rc2.rb
@@ -1,29 +1,35 @@
 @balance = 0
-count = 10000
+count = 1000000
 t1 = Thread.start do
-  count.times do
-    @balance = @balance + 100
-    sleep 0.0000001
+  @lock = Mutex.new
+  @lock.synchronize do
+    count.times do
+      @balance = @balance + 100
+      sleep 0.0000001
+    end
+    puts "[t1] Balance is $#{@balance}      (after adding $100 x #{count})\n"
+    count.times do
+      @balance = @balance - 100
+    end
+    sleep(rand(0..2))
+    puts "[t1] Balance is $#{@balance}     (after subtracting $100 x #{count})\n"
   end
-  puts "[t1] Balance is $#{@balance}      (after adding $100 x #{count})\n"
-  count.times do
-    @balance = @balance - 100
-  end
-  sleep(rand(0..2))
-  puts "[t1] Balance is $#{@balance}     (after subtracting $100 x #{count})\n"
 end
 
 t2 = Thread.start do
-  count.times do
-    @balance = @balance + 100
+  @lock1 = Mutex.new
+  @lock1.synchronize do
+    count.times do
+      @balance = @balance + 100
+    end
+    puts "[t2] Balance is $#{@balance}       (after adding $100 x #{count})\n"
+    count.times do
+      @balance = @balance - 100
+      sleep 0.0000001
+    end
+    sleep(rand(0..2))
+    puts "[t1] Balance is $#{@balance}      (after subtracting $100 x #{count})\n"
   end
-  puts "[t2] Balance is $#{@balance}       (after adding $100 x #{count})\n"
-  count.times do
-    @balance = @balance - 100
-    sleep 0.0000001
-  end
-  sleep(rand(0..2))
-  puts "[t1] Balance is $#{@balance}      (after subtracting $100 x #{count})\n"
 end
 
 t1.join
